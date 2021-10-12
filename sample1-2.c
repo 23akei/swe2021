e1210a1aa43774d378abbf5c587d610ebc5c330c
Akei Kanzaki <tom0pr01fr.olem.001@gmail.com>
2021-10-12 14:31:23 +0900

    add 4 C files

---
+++ sample1-2.c
@@ -0,0 +1,10 @@
+// sample 1‐2
+#include <stdio.h>
+int main(){
+    int i = 10 / 3;
+    float j = 10.5 / 3.317;
+    char k = 'b' + 9;
+    printf("i = %d \n", i);
+    printf("j = %f \n", j);
+    printf("k = %c \n", k);
+}
